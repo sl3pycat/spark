07e04032b330c08ab6f0bac4a5b8616ce3cb8104
sl3pycat@gmail.com <sl3pycat@gmail.com>
2023-11-28 06:20:47 +0200

    init

--- spark.js
+++ spark.js
@@ -1,1 +1,10 @@
-alert("hello!")
+const spark = {
+  // a collection of all scripts that should be loaded on init (push to the array to add custom libs)
+  libs:["core/setup.js"],
+  
+}
+
+//wait for the application to fully load before doing anything ( for good pwa scores )
+document.addEventListener("DOMContentLoaded",()=>{
+  
+})
